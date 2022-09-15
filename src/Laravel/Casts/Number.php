f0492ad24654d3f6d6a76e442219712d5d4b7ef2
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2022-09-15 15:01:07 +0430

    Fix code-style

--- src/Laravel/Casts/Number.php
+++ src/Laravel/Casts/Number.php
@@ -1,40 +1,44 @@
 <?php
+
 namespace dnj\Number\Laravel\Casts;
 
 use dnj\Number\Contracts\INumber;
 use dnj\Number\Number as NumberFacade;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
-class Number implements CastsAttributes {
+class Number implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param mixed                               $value
+     * @param array                               $attributes
+     *
+     * @return INumber
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return NumberFacade::fromInput($value);
+    }
 
-	/**
-	 * Cast the given value.
-	 *
-	 * @param  \Illuminate\Database\Eloquent\Model $model
-	 * @param  string  $key
-	 * @param  mixed  $value
-	 * @param  array  $attributes
-	 * @return INumber
-	 */
-	public function get($model, $key, $value, $attributes)
-	{
-		return NumberFacade::fromInput($value);
-	}
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param INumber                             $value
+     * @param array                               $attributes
+     *
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        if ($value instanceof INumber) {
+            return $value->__toString();
+        }
 
-	/**
-	 * Prepare the given value for storage.
-	 *
-	 * @param  \Illuminate\Database\Eloquent\Model  $model
-	 * @param  string  $key
-	 * @param  INumber  $value
-	 * @param  array  $attributes
-	 * @return string
-	 */
-	public function set($model, $key, $value, $attributes)
-	{
-		if ($value instanceof INumber) {
-			return $value->__toString();
-		}
-		return $value;
-	}
+        return $value;
+    }
 }
--- tests/NumberTraitTest.php
+++ tests/NumberTraitTest.php
@@ -1,17 +1,20 @@
 <?php
+
 namespace dnj\Number\Tests;
 
 use dnj\Number\Number;
 use PHPUnit\Framework\TestCase;
 
-class NumberTraitTest extends TestCase {
-	public function test() {
-		$a = Number::fromInput(0.1);
-		$b = Number::fromInput(0.2);
-		$this->assertTrue($b->gt($a));
-		$this->assertTrue($b->gte($a));
-		$this->assertTrue($a->lt($b));
-		$this->assertTrue($b->lte($b));
-		$this->assertTrue($b->eq($b));
-	}
+class NumberTraitTest extends TestCase
+{
+    public function test()
+    {
+        $a = Number::fromInput(0.1);
+        $b = Number::fromInput(0.2);
+        $this->assertTrue($b->gt($a));
+        $this->assertTrue($b->gte($a));
+        $this->assertTrue($a->lt($b));
+        $this->assertTrue($b->lte($b));
+        $this->assertTrue($b->eq($b));
+    }
 }
